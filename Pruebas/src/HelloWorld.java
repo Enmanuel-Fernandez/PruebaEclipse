e754f79893379285f4feb503631eac245ac60ac9
Enmanuel-Fernandez <72040545+Enmanuel-Fernandez@users.noreply.github.com>
2020-12-23 18:34:30 +0100

    Commit de prueba

--- Pruebas/src/HelloWorld.java
+++ Pruebas/src/HelloWorld.java
@@ -1,6 +1,26 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World v2");
+		int[] estado = {1,1};
+		int[] esucesor = {2,0};
+		
+		boolean[] vecinos =  {true, true, false, true};
+		Celda cs = new Celda(2, 0, 3, vecinos);
+		
+		//System.out.println(cs.toString());
+		
+		int coste = (((estado[0] % 2)+1) * ((cs.getValor())+1));
+		
+		System.out.println(((estado[0] % 2)+1));
+		System.out.println(((cs.getValor())+1));
+		System.out.println(coste);
 	}
 }
